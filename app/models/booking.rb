8a5de242eaa5750d69d13b7443271b4a7555fa87
Perrine Dohr <antartikat@gmail.com>
2024-02-14 18:48:40 +0100

    validations ok

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,8 @@
 class Booking < ApplicationRecord
   belongs_to :hero
   belongs_to :user
+  validates :booking_number, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :total_price, presence: true
 end
--- app/models/heroe.rb
+++ app/models/heroe.rb
@@ -1,3 +1,9 @@
 class Heroe < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, through: :bookings
+  has_many :bookings, dependent: :destroy
+  has_one_attached :photo
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :available, inclusion: { in: [true, false] }
+  validates :price_per_day, presence: true
 end
