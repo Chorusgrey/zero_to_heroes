ffb440ebee87e7cb139f21c23354b4065c69bbd0
BarjaKarim <karim.barja@gmail.com>
2024-02-15 21:32:23 +0100

    route modifier pour la home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, except: [:index]
+  skip_before_action :authenticate_user!, only: [:home]
 
   def home
   end
