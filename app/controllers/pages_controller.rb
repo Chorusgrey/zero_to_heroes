02d8e73c2e9d1fbe5609a22cfa07a128649f5cf1
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-02-19 18:48:57 +0100

    Profil user

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,11 @@ class PagesController < ApplicationController
   def home
     @heros = Hero.all
   end
+
+  def dashboard
+    @bookings_as_tenant = current_user.bookings_as_tenant
+    @bookings_as_owner = current_user.bookings_as_owner
+    @heros_for_rent = current_user.heros
+  end
+
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,8 +1,12 @@
 class Booking < ApplicationRecord
-  belongs_to :heroe
+  belongs_to :hero
   belongs_to :user
-  validates :booking_number, presence: true
-  validates :start_date, presence: true
-  validates :end_date, presence: true
-  validates :total_price, presence: true
+  # validates :booking_number, presence: true
+  # validates :start_date, presence: true
+  # validates :end_date, presence: true
+  # validates :total_price, presence: true
+
+  def total_price
+    hero.price_per_day * (end_date - start_date + 1).to_i
+  end
 end
