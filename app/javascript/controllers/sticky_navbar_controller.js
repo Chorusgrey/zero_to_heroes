7e9c07fe81dd93a6ac2608b91096e04e6bc71943
BarjaKarim <karim.barja@gmail.com>
2024-02-24 15:18:19 +0100

    js controller remove

--- app/javascript/controllers/sticky_navbar_controller.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from "@hotwired/stimulus";
-
-export default class extends Controller {
-  static targets = ["navbar"];
-
-  connect() {
-    this.navbarTarget.classList.add("fixed");
-  }
-}
