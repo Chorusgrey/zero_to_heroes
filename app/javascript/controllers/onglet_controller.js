44a19dcf545e5a4b0a36f5c7f21d18f1c5a9c7ff
BarjaKarim <karim.barja@gmail.com>
2024-02-27 18:14:01 +0100

    modif  onglets

--- app/javascript/controllers/onglet_controller.js
+++ app/javascript/controllers/onglet_controller.js
@@ -1,8 +1,8 @@
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="onglet"
+
 export default class extends Controller {
-  static targets = [ "reservations", "heros" ];
+  static targets = [ "reservations", "heros", "booked" ];
 
   connect() {
     console .log('connected');
@@ -13,27 +13,21 @@ export default class extends Controller {
     const heros = document.querySelector('.list-heros');
 
     if (reservations.classList.contains('unvisible')) {
-      console.log('1');
       reservations.classList.remove('unvisible');
       reservations.classList.add('visible');
       heros.classList.add('unvisible');
     } else {
-      console.log('2');
       reservations.classList.add('unvisible');
       reservations.classList.remove('visible');
       heros.classList.remove('unvisible');
     }
-
     if (heros.classList.contains('unvisible')) {
-      console.log('3');
       heros.classList.remove('visible');
       reservations.classList.remove('unvisible');
     } else {
-      console.log('4');
       heros.classList.remove('unvisible');
       reservations.classList.add('visible');
     }
     // this.herosTarget.classList.toggle('unvisible');
   }
-
 }
