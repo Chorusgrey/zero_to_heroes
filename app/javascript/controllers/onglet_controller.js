2ba5e503e58ec820c7223129d1486d95c5e2a6c1
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-02-27 19:29:49 +0100

    Onglet ok

--- app/javascript/controllers/onglet_controller.js
+++ app/javascript/controllers/onglet_controller.js
@@ -9,19 +9,28 @@ export default class extends Controller {
   }
 
   reservations (event) {
-
+    console.log(this.reservationsTarget)
     this.reservationsTarget.classList.add('visible')
+    this.reservationsTarget.classList.remove('unvisible')
     this.herosTarget.classList.add('unvisible');
+    this.herostarget.classList.remove('visible');
     this.bookedTarget.classList.add('unvisible');
+    this.bookedTarget.classList.remove('visible');
   }
   heros (event) {
     this.reservationsTarget.classList.add('unvisible')
+    this.reservationsTarget.classList.remove('visible')
     this.herosTarget.classList.add('visible');
+    this.herosTarget.classList.remove('unvisible');
     this.bookedTarget.classList.add('unvisible');
+    this.bookedTarget.classList.remove('visible');
   }
   booked (event) {
     this.reservationsTarget.classList.add('unvisible')
+    this.reservationsTarget.classList.remove('visible')
     this.herosTarget.classList.add('unvisible');
+    this.herosTarget.classList.remove('visible');
     this.bookedTarget.classList.add('visible');
+    this.bookedTarget.classList.remove('unvisible');
   }
 }
